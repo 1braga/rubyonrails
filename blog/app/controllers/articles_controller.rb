4b857ad59b44419ab594a93c21209b5334f5c93b
Giovanna Braga Sória <gbraga.soria@gmail.com>
2024-11-14 17:22:47 -0400

    feat: 11 security full

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  https_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
